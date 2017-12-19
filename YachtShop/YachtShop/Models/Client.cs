d97818f1b11617dc5cc288daafebb98886522d9b
Marek Przybolewski <mprzybolewski@gmail.com>
2017-12-19 22:22:02 +0100

    add project and basic models

---
+++ YachtShop/YachtShop/Models/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YachtShop.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        [Required(ErrorMessage = "Enter First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Enter Second Name")]
+        public string SecondName { get; set; }
+        [Required(ErrorMessage = "Enter Phone Number")]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Enter Email")]
+        public string Email { get; set; }
+
+}
+}
---
+++ YachtShop/YachtShop/Pages/Client.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace YachtShop.Pages
+{
+    public class ClientModel : PageModel
+    {
+        public string WelcomeString { get; set; }
+        public void OnGet()
+        {
+            WelcomeString = "Welcome test";
+        }
+    }
+}
