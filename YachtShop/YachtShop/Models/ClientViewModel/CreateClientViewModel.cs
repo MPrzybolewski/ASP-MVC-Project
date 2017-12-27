65a130db163843a33a255e2cc422100f243560f4
Marek Przybolewski <mprzybolewski@gmail.com>
2017-12-27 13:23:23 +0100

    add ViewModels for client

---
+++ YachtShop/YachtShop/Models/ClientViewModel/CreateClientViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YachtShop.Models.ClientViewModel
+{
+    public class CreateClientViewModel
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and max {1} characters long", MinimumLength = 3)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and max {1} characters long", MinimumLength = 3)]
+        [Display(Name = "Second Name")]
+        public string SecondName { get; set; }
+        [Required]
+        [StringLength(9, ErrorMessage = "The {0} must be at least {2} and max {1} characters long", MinimumLength = 9)]
+        [Display(Name = "Second Name")]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [Display(Name = "Second Name")]
+        public string Email { get; set; }
+    }
+}
---
+++ YachtShop/YachtShop/Models/ClientViewModel/DetailClientViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YachtShop.Models.ClientViewModel
+{
+    public class DetailClientViewModel
+    {
+        public string ClientId { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
