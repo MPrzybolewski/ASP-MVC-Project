fb013bbc89c258f0dcdca1807ce8b86ebc885f82
Marek Przybolewski <mprzybolewski@gmail.com>
2017-12-29 15:18:40 +0100

    add CRUD for Purchase model

---
+++ YachtShop/YachtShop/Controllers/PurchaseController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using YachtShop.Data;
+using YachtShop.Models;
+
+namespace YachtShop.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PurchaseController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Purchase
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Purchases.Include(p => p.Client).Include(p => p.Seller).Include(p => p.Yacht);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Purchase/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var purchase = await _context.Purchases
+                .Include(p => p.Client)
+                .Include(p => p.Seller)
+                .Include(p => p.Yacht)
+                .SingleOrDefaultAsync(m => m.PurchaseId == id);
+            if (purchase == null)
+            {
+                return NotFound();
+            }
+
+            return View(purchase);
+        }
+
+        // GET: Purchase/Create
+        public IActionResult Create()
+        {
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "ClientId");
+            ViewData["SellerId"] = new SelectList(_context.Sellers, "SellerId", "SellerId");
+            ViewData["YachtId"] = new SelectList(_context.Yachts, "YachtId", "YachtId");
+            return View();
+        }
+
+        // POST: Purchase/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PurchaseId,ClientId,SellerId,YachtId,PurchaseDate")] Purchase purchase)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(purchase);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "ClientId", purchase.ClientId);
+            ViewData["SellerId"] = new SelectList(_context.Sellers, "SellerId", "SellerId", purchase.SellerId);
+            ViewData["YachtId"] = new SelectList(_context.Yachts, "YachtId", "YachtId", purchase.YachtId);
+            return View(purchase);
+        }
+
+        // GET: Purchase/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var purchase = await _context.Purchases.SingleOrDefaultAsync(m => m.PurchaseId == id);
+            if (purchase == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "ClientId", purchase.ClientId);
+            ViewData["SellerId"] = new SelectList(_context.Sellers, "SellerId", "SellerId", purchase.SellerId);
+            ViewData["YachtId"] = new SelectList(_context.Yachts, "YachtId", "YachtId", purchase.YachtId);
+            return View(purchase);
+        }
+
+        // POST: Purchase/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("PurchaseId,ClientId,SellerId,YachtId,PurchaseDate")] Purchase purchase)
+        {
+            if (id != purchase.PurchaseId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(purchase);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PurchaseExists(purchase.PurchaseId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "ClientId", purchase.ClientId);
+            ViewData["SellerId"] = new SelectList(_context.Sellers, "SellerId", "SellerId", purchase.SellerId);
+            ViewData["YachtId"] = new SelectList(_context.Yachts, "YachtId", "YachtId", purchase.YachtId);
+            return View(purchase);
+        }
+
+        // GET: Purchase/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var purchase = await _context.Purchases
+                .Include(p => p.Client)
+                .Include(p => p.Seller)
+                .Include(p => p.Yacht)
+                .SingleOrDefaultAsync(m => m.PurchaseId == id);
+            if (purchase == null)
+            {
+                return NotFound();
+            }
+
+            return View(purchase);
+        }
+
+        // POST: Purchase/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var purchase = await _context.Purchases.SingleOrDefaultAsync(m => m.PurchaseId == id);
+            _context.Purchases.Remove(purchase);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PurchaseExists(string id)
+        {
+            return _context.Purchases.Any(e => e.PurchaseId == id);
+        }
+    }
+}
