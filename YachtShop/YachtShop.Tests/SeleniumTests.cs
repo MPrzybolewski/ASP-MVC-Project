2e27c839bd61760e9d6a02812f1b376ff95fd6c6
Marek Przybolewski <mprzybolewski@gmail.com>
2018-05-11 00:04:35 +0200

    add ui tests

--- YachtShop/YachtShop.Tests/SeleniumTests.cs
+++ YachtShop/YachtShop.Tests/SeleniumTests.cs
@@ -10,7 +10,7 @@ using Xunit;
 
 namespace YachtShop.Tests
 {
-    public class SeleniumTests 
+    public class SeleniumTests
     {
         [Fact]
         public void HomeViewPartial_ShouldReturnCorrectString_WhenUserIsNotLoggedIn()
@@ -53,6 +53,271 @@ namespace YachtShop.Tests
 
             Assert.Equal("The Email field is required.", titleString);
             driver.Close();
+        }
+
+        [Fact]
+        public void LoginView_ShouldShowCorrectString_WhenUserPassWrongEmail()
+        {
+            var driver = new ChromeDriver(Directory.GetCurrentDirectory());
+            var url = "https://localhost:44373/account/login";
+            driver.Navigate().GoToUrl(url);
+
+            driver.FindElement(By.Id("Email")).SendKeys("asdfasdfsad");
+
+            driver.FindElement(By.Id("loginButtonForm")).Click();
+
+            var titleString = driver.FindElement(By.Id("wrongEmail")).Text;
+            //jaki string
+            Assert.Equal("The Email field is not a valid e-mail address.", titleString);
+            driver.Close();
+        }
+
+        [Fact]
+        public void LoginView_ShouldShowCorrectString_WhenUserPassPasswordNotBelongToEmail()
+        {
+            var driver = new ChromeDriver(Directory.GetCurrentDirectory());
+            var url = "https://localhost:44373/account/login";
+            driver.Navigate().GoToUrl(url);
+
+            driver.FindElement(By.Id("Email")).SendKeys("seller@seller.pl");
+
+            driver.FindElement(By.Id("Password")).SendKeys("aaaaaa");
+
+            driver.FindElement(By.Id("loginButtonForm")).Click();
+
+            var titleString = driver.FindElement(By.Id("validationString")).Text;
+            //jaki string
+            Assert.Equal("Invalid login attempt.", titleString);
+            driver.Close();
+        }
+
+
+        [Fact]
+        public void HomePage_ShouldShowCorrectWelocmeString_WhenUserLogInCorrectly()
+        {
+            var driver = new ChromeDriver(Directory.GetCurrentDirectory());
+            var url = "https://localhost:44373/account/login";
+            driver.Navigate().GoToUrl(url);
+
+            driver.FindElement(By.Id("Email")).SendKeys("123@123.pl");
+
+            driver.FindElement(By.Id("Password")).SendKeys("1234");
+            driver.FindElement(By.Id("loginButtonForm")).Click();
+
+
+            var titleString = driver.FindElement(By.Id("WelcomeString")).Text;
+            //jaki string
+            Assert.Contains("123@123.pl", titleString);
+            driver.Close();
+        }
+        
+
+        [Fact]
+        public void HomePage_ShouldShowYachtTab_WhenUserIsNotLogged()
+        {
+            var driver = new ChromeDriver(Directory.GetCurrentDirectory());
+            var url = "https://localhost:44373/";
+            driver.Navigate().GoToUrl(url);
+
+            var tabText = driver.FindElement(By.Id("yachtTab")).Text;
+            Assert.Equal("Yachts", tabText);
+            driver.Close();
+        }
+
+        [Fact]
+        public void HomePage_ShouldNotShowClientTab_WhenUserIsNotLoggedAsASeller()
+        {
+            var driver = new ChromeDriver(Directory.GetCurrentDirectory());
+            var url = "https://localhost:44373/";
+            driver.Navigate().GoToUrl(url);
+            
+            Assert.Throws<NoSuchElementException>(() => driver.FindElement(By.Id("sellerTab")));
+            driver.Close();
+        }
+
+        [Fact]
+        public void HomePage_ShouldShowSellerTab_WhenUserIsLoggedAsAdmin()
+        {
+            var driver = new ChromeDriver(Directory.GetCurrentDirectory());
+            var url = "https://localhost:44373/account/login";
+            driver.Navigate().GoToUrl(url);
+            Login(driver);
+
+            var tabText = driver.FindElement(By.Id("sellerTab")).Text;
+            Assert.Equal("Sellers", tabText);
+            driver.Close();
+        }
+
+        [Fact]
+        public void HomePage_ShouldNavigateToSellerView_WhenAdminClickSellerTab()
+        {
+            var driver = new ChromeDriver(Directory.GetCurrentDirectory());
+            var url = "https://localhost:44373/account/login";
+            driver.Navigate().GoToUrl(url);
+            Login(driver);
+
+            driver.FindElement(By.Id("sellerTab")).Click();
+
+            var title = driver.FindElement(By.Id("sellerPageIndexTitle")).Text;
+            Assert.Equal("Seller", title);
+            driver.Close();
+        }
+
+        [Fact]
+        public void SellerIndexPage_ShouldShowCorrectTabs_WhenSellerClickSellerTab()
+        {
+            var driver = new ChromeDriver(Directory.GetCurrentDirectory());
+            var url = "https://localhost:44373/account/login";
+            driver.Navigate().GoToUrl(url);
+            Login(driver);
+
+            driver.FindElement(By.Id("sellerTab")).Click();
+
+            string[] tabsText = new string[5];
+            tabsText[0] = driver.FindElement(By.Id("firstNameCol")).Text;
+            tabsText[1] = driver.FindElement(By.Id("secondNameCol")).Text;
+            tabsText[2] = driver.FindElement(By.Id("salaryCol")).Text;
+            tabsText[3] = driver.FindElement(By.Id("phoneNumberCol")).Text;
+            tabsText[4] = driver.FindElement(By.Id("emailCol")).Text;
+
+            string[] expectedText = new string[]
+            {
+                "First Name",
+                "Second Name",
+                "Salary",
+                "Phone Number",
+                "Email"
+            };
+
+
+            Assert.Equal(expectedText, tabsText);
+            driver.Close();
+        }
+
+        [Fact]
+        public void SellerPageIndex_ShouldNavigateToSellerCreatePage_WhenAdminClickCreateButton()
+        {
+            var driver = new ChromeDriver(Directory.GetCurrentDirectory());
+            var url = "https://localhost:44373/account/login";
+            driver.Navigate().GoToUrl(url);
+            Login(driver);
+
+            driver.FindElement(By.Id("sellerTab")).Click();
+
+            driver.FindElement(By.Id("createButton")).Click();
+
+            string[] tabsText = new string[5];
+            tabsText[0] = driver.FindElement(By.Id("firstNameLabel")).Text;
+            tabsText[1] = driver.FindElement(By.Id("secondNameLabel")).Text;
+            tabsText[2] = driver.FindElement(By.Id("salaryLabel")).Text;
+            tabsText[3] = driver.FindElement(By.Id("phoneNumberLabel")).Text;
+            tabsText[4] = driver.FindElement(By.Id("emailLabel")).Text;
+
+            string[] expectedText = new string[]
+            {
+                "First Name",
+                "Second Name",
+                "Salary",
+                "Phone Number",
+                "Email"
+            };
+
+            Assert.Equal(expectedText, tabsText);
+            driver.Close();
+        }
+
+        [Fact]
+        public void SellerPageCreate_ShouldShowAllForms_WhenAdminIsOnPage()
+        {
+            var driver = new ChromeDriver(Directory.GetCurrentDirectory());
+            var url = "https://localhost:44373/account/login";
+            driver.Navigate().GoToUrl(url);
+            Login(driver);
+
+            driver.FindElement(By.Id("sellerTab")).Click();
+
+            driver.FindElement(By.Id("createButton")).Click();
+
+            string titleText = driver.FindElement(By.Id("firstNameLabel")).Text;
+            Assert.Equal("First Name", titleText);
+            driver.Close();
+        }
+        
+
+       [Fact]
+        public void SellerPageCreate_ShouldShowCorrectString_WhenAdminPutWrongSalary()
+        {
+            var driver = new ChromeDriver(Directory.GetCurrentDirectory());
+            var url = "https://localhost:44373/account/login";
+            driver.Navigate().GoToUrl(url);
+            Login(driver);
+
+            driver.FindElement(By.Id("sellerTab")).Click();
+
+            driver.FindElement(By.Id("createButton")).Click();
+
+            driver.FindElement(By.Id("Salary")).SendKeys("test");
+            driver.FindElement(By.Id("createButton")).Click();
+            
+            var text = driver.FindElement(By.Id("salaryTextDanger")).Text;
+
+            Assert.Contains("not valid for Salary.", text);
+            driver.Close();
+        }
+
+        [Fact]
+        public void SellerPageCreate_ShouldShowCorrectString_WhenAdminPutNullToFirstName()
+        {
+            var driver = new ChromeDriver(Directory.GetCurrentDirectory());
+            var url = "https://localhost:44373/account/login";
+            driver.Navigate().GoToUrl(url);
+            Login(driver);
+
+            driver.FindElement(By.Id("sellerTab")).Click();
+
+            driver.FindElement(By.Id("createButton")).Click();
+
+            driver.FindElement(By.Id("Salary")).SendKeys("test");
+            driver.FindElement(By.Id("createButton")).Click();
+
+            var text = driver.FindElement(By.Id("firstNameTextDanger")).Text;
+
+            Assert.Contains("Enter First Name", text);
+            driver.Close();
+        }
+
+        [Fact]
+        public void SellerPageCreate_ShouldNavigateToIndexPage_WhenAdminPutCorrectData()
+        {
+            var driver = new ChromeDriver(Directory.GetCurrentDirectory());
+            var url = "https://localhost:44373/account/login";
+            driver.Navigate().GoToUrl(url);
+            Login(driver);
+
+            driver.FindElement(By.Id("sellerTab")).Click();
+
+            driver.FindElement(By.Id("createButton")).Click();
+
+            driver.FindElement(By.Id("FirstName")).SendKeys("test");
+            driver.FindElement(By.Id("SecondName")).SendKeys("test");
+            driver.FindElement(By.Id("Salary")).SendKeys("2000");
+            driver.FindElement(By.Id("PhoneNumber")).SendKeys("123-123-123");
+            driver.FindElement(By.Id("Email")).SendKeys("test@test.pl");
+
+            driver.FindElement(By.Id("createButton")).Click();
+
+            var text = driver.FindElement(By.Id("sellerPageIndexTitle")).Text;
+
+            Assert.Contains("Seller", text);
+            driver.Close();
+        }
+
+        void Login(ChromeDriver driver)
+        {
+            driver.FindElement(By.Id("Email")).SendKeys("123@123.pl");
+
+            driver.FindElement(By.Id("Password")).SendKeys("1234");
+            driver.FindElement(By.Id("loginButtonForm")).Click();
         }
     }
 }
---
+++ YachtShop/YachtShop.UICodeTests/CodedUITest1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+
+
+namespace YachtShop.UICodeTests
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest]
+    public class CodedUITest1
+    {
+        public CodedUITest1()
+        {
+        }
+
+        [TestMethod]
+        public void CheckCorrectLogInAsAdmin()
+        {
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            this.UIMap.RunHomePage();
+            this.UIMap.ClickLogInTabButton();
+            this.UIMap.AddAdminEmail();
+            this.UIMap.AddCorrectPassword();
+            this.UIMap.ClickLogInButton();
+            this.UIMap.ValidateWelcomeStringAfterLogin();
+            this.UIMap.LogOut();
+        }
+
+
+        [TestMethod]
+        public void Admin_CanAddNewSeller()
+        {
+            this.UIMap.RunHomePage();
+            this.UIMap.ClickLogInTabButton();
+            this.UIMap.AddAdminEmail();
+            this.UIMap.AddCorrectPassword();
+            this.UIMap.ClickLogInButton();
+            this.UIMap.NavigateToSellerIndexPage();
+            this.UIMap.ClickCreateNewSeller();
+            this.UIMap.AddCorrectSeller();
+            this.UIMap.ClickCreateNewSellerButton();
+            this.UIMap.ValidateSellerFirstName();
+            this.UIMap.ValidateSellerSecondName();
+            this.UIMap.ValidateSellerSalary();
+            this.UIMap.ClickDetailSellerButton();
+            this.UIMap.VerifyDetailPage();
+            this.UIMap.ClickEditSellerButton();
+            this.UIMap.EditSellerFirstName();
+            this.UIMap.SaveEditSeller();
+            this.UIMap.VerifySellerFirstNameAfterEdit();
+            this.UIMap.ClieckSellerDeleteButton();
+            this.UIMap.ClickDeleteSellerButton();
+            this.UIMap.LogOut();
+        }
+        [TestMethod]
+        public void Admin_CheckRolePage()
+        {
+            this.UIMap.RunHomePage();
+            this.UIMap.ClickLogInTabButton();
+            this.UIMap.AddAdminEmail();
+            this.UIMap.AddCorrectPassword();
+            this.UIMap.ClickLogInButton();
+            this.UIMap.ClickRoleTab();
+            this.UIMap.ValidateRoleEmail();
+            this.UIMap.ValidateRole();
+            this.UIMap.LogOut();
+        }
+
+        [TestMethod]
+        public void CreateYacht_CheckValidation()
+        {
+            this.UIMap.RunHomePage();
+            this.UIMap.ClickLogInTabButton();
+            this.UIMap.AddAdminEmail();
+            this.UIMap.AddCorrectPassword();
+            this.UIMap.ClickLogInButton();
+            this.UIMap.ClickYachtTab();
+            this.UIMap.ClickCreateNewYacht();
+            this.UIMap.ClickCreateNewYachtButton();
+            this.UIMap.ValidateNullYachtName();
+            this.UIMap.ValidateNullYachtPrice();
+            this.UIMap.ValidateNullYachtDescription();
+            this.UIMap.PutWrongPrice();
+            this.UIMap.ClickCreateNewYachtButton();
+            this.UIMap.ValidateWrongPrice();
+            this.UIMap.LogOut();
+        }
+
+        [TestMethod]
+        public void NotLoggedUser_ShouldNavigateToLoginPage_WhenTypeSellerPage()
+        {
+            this.UIMap.GoToSellerPage();
+            this.UIMap.ValidateLoginPageURL();
+        }
+
+        [TestMethod]
+        public void NotLoggedUser_ShouldSeeCorrectTabs()
+        {
+            this.UIMap.RunHomePage();
+            this.UIMap.ValidateTabsForNotLoggedUser();
+        }
+
+        [TestMethod]
+        public void Admin_ShouldSeeAllTabs()
+        {
+            this.UIMap.RunHomePage();
+            this.UIMap.ClickLogInTabButton();
+            this.UIMap.AddAdminEmail();
+            this.UIMap.AddCorrectPassword();
+            this.UIMap.ClickLogInButton();
+            this.UIMap.ValidateTabsForAdmin();
+            this.UIMap.LogOut();
+        }
+
+        [TestMethod]
+        public void CreateClient_CheckWrongValidation()
+        {
+            this.UIMap.RunHomePage();
+            this.UIMap.ClickLogInTabButton();
+            this.UIMap.AddAdminEmail();
+            this.UIMap.AddCorrectPassword();
+            this.UIMap.ClickLogInButton();
+            this.UIMap.ClickClientTab();
+            this.UIMap.ClickCreateNewClient();
+            this.UIMap.ClickCreateClientButton();
+            this.UIMap.ValidateNullClientFirstName();
+            this.UIMap.ValidateNullClientSecondName();
+            this.UIMap.ValidateNullClientPhoneNumber();
+            this.UIMap.ValidateNullClientEmail();
+            this.UIMap.TypeWronNumberWithoutDash();
+            this.UIMap.ClickCreateClientButton();
+            this.UIMap.ValidateIncorrectPhoneNumber();
+            this.UIMap.TypeWrongEmail();
+            this.UIMap.ClickCreateClientButton();
+            this.UIMap.ValidateIncorrectEmail();
+            this.UIMap.LogOut();
+        }
+
+        [TestMethod]
+        public void CreateClient_CheckCorrectValidation()
+        {
+            this.UIMap.RunHomePage();
+            this.UIMap.ClickLogInTabButton();
+            this.UIMap.AddAdminEmail();
+            this.UIMap.AddCorrectPassword();
+            this.UIMap.ClickLogInButton();
+            this.UIMap.ClickClientTab();
+            this.UIMap.ClickCreateNewClient();
+            this.UIMap.AddCorrectClientData();
+            this.UIMap.ClickCreateClientButton();
+            this.UIMap.ValidateClientIndexPage();
+            this.UIMap.DeleteClient();
+            this.UIMap.LogOut();
+        }
+
+
+        #region Additional test attributes
+
+        // You can use the following additional attributes as you write your tests:
+
+        ////Use TestInitialize to run code before running each test 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        ////Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        #endregion
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if (this.map == null)
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
